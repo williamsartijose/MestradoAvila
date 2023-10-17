8559972de98c57d1d6fbb9b58bc3e969094e98ea
unknown <williamsartijose@hotmail.com>
2023-10-17 19:30:28 +0100

    9. Caso práctico 2 - Ejercicio 2 R

---
+++ Las herramientas del científico de datos/9. Caso práctico 2/teste.R
@@ -0,0 +1,13 @@
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("ggplot2")
+
+library(dplyr)
+
+library(tidyr)
+
+library(ggplot2)
+
+
+df <- read.csv("Airbnb_Milan.csv")
+head(df,10)
